96394e7dc7a5ccaa259d8ac3500a5f89783329e0
Nicole Avila-Rivera <nicoleavila-rivera@Nicoles-MacBook-Pro.local>
2023-02-23 00:32:56 -0600

    fixed button

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ gradient(4,0.1,2).invert().luma().modulatePixelate(noise(25,0.5),100).invert()
 .layer(osc(4,0.1,2).luma().mask(shape(2,0.5).scrollY(-0.25))).out(o0)
 
 const element = document.getElementById("myBtn");
-element.addEventListener("click", myFunction);
 element.addEventListener("click", function() {
+    const track = document.querySelector('audio');
+    track.play();
     console.log("clicked");
   });
